5a06837b05c76ba4b51158ff1bbd0395ad640357
dtrstenja23 <dtrstenja23@student.foi.hr>
2026-05-14 16:29:13 +0200

    Move theme constants to a separate theme.py file

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ import generate
 import sys
 from character_creation import center_window, open_create_character_selector
 from background_creation import open_create_background
+from theme import *
 
 try:
     import ctypes
@@ -28,23 +29,6 @@ pygame.mixer.init()
 DEFAULT_LOCATION = ''
 DEFAULT_BGM = ''
 api_key = ''
-
-BG = "#212326"  
-BG2 = "#0d0a08"  
-ACCENT = "#c0392b"
-ACCENT2 = "#8b1a1a"
-BORDER = "#7a3c14"
-BORDER2 = "#2a1208"
-TEXT = "#f0d9b0"
-TEXT_MUTED = "#a89880"
-TEXT_DIM = "#4a3020"
-
-FONT_CANVAS_TITLE = ("Almendra SC", 48, "bold")
-FONT_CANVAS_LABEL = ("Baskervville SC", 28, "bold")
-FONT_CANVAS_VAL = ("Baskervville SC", 28)
-FONT_CANVAS_BTN = ("Baskervville SC", 24, "bold")
-FONT_CANVAS_ITEM = ("Baskervville SC", 24)
-FONT_CANVAS_SYMBOL = ("Baskervville SC", 22)
 
 # Parse the configuration file
 def parse_config(filename):
@@ -545,7 +529,7 @@ class Application(tk.Tk):
     def on_run(self):
         
         if sys.platform.startswith("win"):
-            renpy_path = r".\renpy-8.5.2-sdk\renpy.exe"
+            renpy_path = r".\renpy-8.3.7-sdk\renpy.exe"
         else:
             renpy_path = "renpy"
  
---
+++ theme.py
@@ -0,0 +1,16 @@
+BG       = "#212326"
+BG2      = "#0d0a08"
+ACCENT   = "#c0392b"
+ACCENT2  = "#8b1a1a"
+BORDER   = "#7a3c14"
+BORDER2  = "#2a1208"
+TEXT     = "#f0d9b0"
+TEXT_MUTED = "#a89880"
+TEXT_DIM   = "#4a3020"
+
+FONT_CANVAS_TITLE  = ("Almendra SC",     48, "bold")
+FONT_CANVAS_LABEL  = ("Baskervville SC", 28, "bold")
+FONT_CANVAS_VAL    = ("Baskervville SC", 28)
+FONT_CANVAS_BTN    = ("Baskervville SC", 24, "bold")
+FONT_CANVAS_ITEM   = ("Baskervville SC", 24)
+FONT_CANVAS_SYMBOL = ("Baskervville SC", 22)
